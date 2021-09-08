cf2fbff55ebae6cc023083cc5f27fe707755f0ad
IDarkstarX <90350962+IDarkstarX@users.noreply.github.com>
2021-09-08 16:48:03 -0500

    onedown

--- GAME2303_sPaskin_week2Assignment1/GAME2303_sPaskin_week2Assignment1.cpp
+++ GAME2303_sPaskin_week2Assignment1/GAME2303_sPaskin_week2Assignment1.cpp
@@ -2,10 +2,66 @@
 //
 
 #include <iostream>
+#include <istream>
+#include <string>
+using namespace std;
 
-int main()
+void twoNums(int x, int y) {
+
+    for (int i = 1; i < 1000; i++) {
+        if (i % x == 0) {
+            if (i % y == 0) {
+                cout << i <<"\n";
+            }
+        }
+    }
+}
+
+void gnirts(string s) {
+
+}
+
+int main() // cout << "\n";
 {
-    std::cout << "Hello World! I'm hungry!\n";
+
+    int menuInput;
+    int num1;
+    int num2;
+    string sin;
+    
+    cout << "Please make a selection using the number in ():\n\n";
+    cout << "(1) Divisible by two numbers.\n";
+    cout << "(2) Reverse string.\n";
+    cout << "(3) Vowel remover.\n\n";
+    cout << "(Any others) Exit.\n\n";
+    cout << "Please make your selection now.\n";
+    cin >> menuInput;
+
+    switch (menuInput)
+    {
+    case 1:
+        cout << "(1) Divisible by two numbers.\n\n";
+        cout << "Please enter first number.\n";
+        cin >> num1;
+        cout << "\nThank you. Please enter second number.\n";
+        cin >> num2;
+        cout << "\nThank you.\n\n";
+        twoNums(num1, num2);
+        break;
+    case 2:
+        cout << "(2) Reverse string.\n\n";
+        cout << "Please enter string to be reversed.\n";
+        cin >> sin;
+        cout << "Thank you.\n";
+        gnirts(sin);
+        break;
+    case 3:
+        break;
+        cout << "(3) Vowel remover.\n\n";
+    default:
+        cout << "(Any others) Exit.";
+        break;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
